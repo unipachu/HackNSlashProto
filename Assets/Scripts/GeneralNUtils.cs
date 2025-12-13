30eb3fe4aaf16d311af491095a2a3519b444f8b7
unipachu <sipinii@hotmail.com>
2025-12-13 02:36:58 +0200

    Unity project added. Player character with simple movement added.

---
+++ Assets/Scripts/GeneralNUtils.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public enum Side
+{
+    Left,
+    Right,
+}
+
+public enum Directions2D
+{
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+public static class GeneralNUtils
+{
+    
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,166 @@
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public enum PlayerState
+{
+    Walking,
+    Attacking,
+}
+
+
+
+public class PlayerController : MonoBehaviour
+{
+
+    [Header("Settings")]
+    [Tooltip("Units per second.")]
+    [SerializeField] private float maxLinearSpeed = 5;
+    [Tooltip("Degrees per second.")]
+    [SerializeField] private float maxAngularSpeed = 360;
+    [Tooltip("Units per second squared.")]
+    [SerializeField] private float acceleration = 100;
+    [Tooltip("In seconds.")]
+    [SerializeField] private float attackDuration = 0.3f;
+
+    [Header("Refs")]
+    [SerializeField] private CharacterController characterController;
+    [SerializeField] private InputActionAsset inputActions;
+    [SerializeField] private InputActionProperty moveActionProperty;
+    [SerializeField] private InputActionProperty attackActionProperty;
+    [SerializeField] private Transform sword;
+
+    private Vector2 velocity = Vector2.zero;
+
+    private Vector2 moveInput = Vector2.zero;
+    private bool attackInput = false;
+    private PlayerState state = PlayerState.Walking;
+    private bool isChangingState = false;
+    private float attackTimer = 0;
+    private Side swordSide = Side.Left;
+
+    private void OnEnable()
+    {
+        inputActions.FindActionMap("Player").Enable();
+    }
+
+    private void Update()
+    {
+        ReadInputs();
+        switch (state)
+        {
+            case PlayerState.Walking:
+                SolveMovement(moveInput);
+                if(attackInput)
+                {
+                    ChangeState(PlayerState.Attacking);
+                    break;
+                }
+                break;
+            case PlayerState.Attacking:
+                float t = attackTimer / attackDuration;
+                float half = attackDuration / 2;
+                if (swordSide == Side.Left)
+                {
+                    Quaternion startRot = Quaternion.Euler(0f, -100f, 0f);
+                    Quaternion endRot = Quaternion.Euler(0f, 100f, 0f);
+                    if (attackTimer / attackDuration >= 1)
+                    {
+                        sword.localRotation = Quaternion.Euler(0f, 90f, 0f);
+                        swordSide = Side.Right;
+                        ChangeState(PlayerState.Walking);
+                        break;
+                    }
+                    if (t < 0.5f)
+                    {
+                        sword.localRotation = Quaternion.Slerp(startRot, Quaternion.identity, attackTimer * 2 / attackDuration);
+                    }
+                    else
+                    {
+                        sword.localRotation = Quaternion.Slerp(Quaternion.identity, endRot, (attackTimer - half) * 2 / attackDuration);
+                    }
+                }
+                else
+                {
+                    Quaternion startRot = Quaternion.Euler(0f, 100f, 0f);
+                    Quaternion endRot = Quaternion.Euler(0f, -100f, 0f);
+                    if (attackTimer / attackDuration >= 1)
+                    {
+                        sword.localRotation = Quaternion.Euler(0f, -90f, 0f);
+                        swordSide = Side.Left;
+                        ChangeState(PlayerState.Walking);
+                        break;
+                    }
+                    if (t < 0.5f)
+                    {
+                        sword.localRotation = Quaternion.Slerp(startRot, Quaternion.identity, attackTimer * 2 / attackDuration);
+                    }
+                    else
+                    {
+                        sword.localRotation = Quaternion.Slerp(Quaternion.identity, endRot, (attackTimer - half) * 2 / attackDuration);
+                    }
+                }
+                attackTimer += Time.deltaTime;
+                break;
+            default:
+                Debug.LogError("Switch defaulted.");
+                break;
+        }
+    }
+
+
+    private void OnDisable()
+    {
+        inputActions.FindActionMap("Player").Disable();
+    }
+
+    private void ChangeState(PlayerState newState)
+    {
+        Debug.Assert(!isChangingState, "Tried to change state while already changing state.");
+        isChangingState = true;
+
+        switch (newState)
+        {
+            case PlayerState.Walking:
+                break;
+            case PlayerState.Attacking:
+                attackTimer = 0;
+                break;
+            default:
+                Debug.LogError("Switch defaulted.");
+                break;
+        }
+
+        state = newState;
+        isChangingState = false;
+    }
+
+    private void ReadInputs()
+    {
+        moveInput = moveActionProperty.action.ReadValue<Vector2>();
+        attackInput = attackActionProperty.action.WasPressedThisFrame();
+    }
+
+    private void SolveMovement(Vector2 movementInput)
+    {
+        UpdateVelocity(movementInput);
+        characterController.SimpleMove(new Vector3(velocity.x, 0, velocity.y));
+        RotateForward();
+    }
+
+    private void UpdateVelocity(Vector2 movementInput)
+    {
+        velocity = Vector2.MoveTowards(velocity, movementInput * maxLinearSpeed, acceleration * Time.deltaTime);
+    }
+
+    public void RotateForward()
+    {
+        if(velocity ==  Vector2.zero) return;
+
+        Vector3 dir3D = new(velocity.x, 0, velocity.y);
+
+        Quaternion targetRotation = Quaternion.LookRotation(dir3D, Vector3.up);
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, maxAngularSpeed * Time.deltaTime);
+    }
+}
